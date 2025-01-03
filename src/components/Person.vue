324f7d01160660581c689ea3c02968d17cb45c90
XianZS <XianZhiSen_yang@outlook.com>
2025-01-03 14:26:19 +0800

    XianzS

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -8,12 +8,25 @@
 
 <script setup lang="ts" name="Person">
 import {ref} from 'vue'
-import {type Person_inter} from "@/types";
+import {type Person_inter, Persons} from "@/types";
 
 let person: Person_inter = {id: "1001", name: '张三', age: 18}
-function getPerson(){
-    console.log(person);
+
+function getPerson() {
+    console.log("person : ", person);
+    console.log("personList : ", personList);
 }
+
+let personList: Array<Person_inter> = [
+    {id: "num1001", name: "jom", age: 18},
+    {id: "num1002", name: "kom", age: 55},
+    {id: "num1003", name: "lom", age: 46},
+]
+let personList1: Persons = [
+    {id: "num1001", name: "jom", age: 18},
+    {id: "num1002", name: "kom", age: 55},
+    {id: "num1003", name: "lom", age: 46},
+]
 </script>
 
 <style scoped>
--- src/types/index.ts
+++ src/types/index.ts
@@ -4,3 +4,10 @@ export interface Person_inter {
 	name: string;
 	age: number;
 }
+
+// 一个自定义类型
+// 写法（一）
+// export type Persons = Array<Person_inter>;
+// 写法（二）
+export type Persons = Person_inter[];
+
